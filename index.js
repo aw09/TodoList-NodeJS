68c3411a90292c0941e8faa4b06f899b084c09e8
Agung Wicaksono <agungwck99@gmail.com>
2023-01-26 08:37:41 +0700

    feat: add todos

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser');
 const db = require('./db')
+const url = require('url')
 
 const app = express()
 const port = 3030
@@ -10,17 +11,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-
-
 // ============= Activities =============
 
-const createActivity = (data) => {
+const activityObject = (data) => {
   return {
-    "created_at": data.created_at,
-    "updated_at": data.updated_at,
     "id": data.activity_id,
     "title": data.title,
-    "email": data.email
+    "email": data.email,
+    "createdAt": data.created_at,
+    "updatedAt": data.updated_at,
   }
 }
 
@@ -48,7 +47,7 @@ app.post('/activity-groups', async (req, res) => {
     res.send({
       "status": "Success",
       "message": "Success",
-      "data": createActivity(row)
+      "data": activityObject(row)
     })
   } else {
     res.status(400).send({
@@ -67,7 +66,7 @@ app.get('/activity-groups', async (req, res) => {
     res.send({
       "status": "Success",
       "message": "Success",
-      "data": result.map((row) => createActivity(row))
+      "data": result.map((row) => activityObject(row))
     })
 })
 
@@ -82,7 +81,7 @@ app.get('/activity-groups/:id', async (req, res) => {
     res.send({
       "status": "Success",
       "message": "Success",
-      "data": createActivity(result[0])
+      "data": activityObject(result[0])
     })
   }
 })
@@ -107,7 +106,7 @@ app.patch('/activity-groups/:id', async (req, res) => {
     res.send({
       "status": "Success",
       "message": "Success",
-      "data": createActivity(row)
+      "data": activityObject(row)
     })
   } else {
     res.status(400).send({
@@ -135,6 +134,182 @@ app.delete('/activity-groups/:id', async (req, res) => {
 })
 
 
+
+// ============= Todos =============
+
+
+const todoObject = (data) => {
+  return {
+    "id": data.todo_id,
+    "activity_group_id": data.activity_group_id,
+    "title": data.title,
+    "priority": data.priority,
+    "is_active": data.is_active == 1 ? true : false,
+    "created_at": data.created_at,
+    "updated_at": data.updated_at,
+  }
+}
+
+const todoNotFound = (req, res) => {
+  res.status(404).send({
+    "status": "Not Found",
+    "message": `Todo with ID ${req.params.id} Not Found`,
+    "data": {}
+  })
+}
+
+// Create
+app.post('/todo-items', async (req, res) => {
+  const title = req.body.title;
+  const groupId = parseInt(req.body.activity_group_id);
+  const priority = req.body.priority || "very-high";
+  const isActive = req.body.is_active;
+  console.log(req.body);
+  
+
+  if (title && groupId) {
+    const param = [title, groupId, priority]
+    let sqlColumn = 'title, activity_group_id, priority'
+    let sqlValues = '?, ?, ?'
+
+    if(typeof isActive != 'undefined'){
+      sqlColumn = sqlColumn.concat(', is_active');
+      sqlValues = sqlValues.concat(', ?');
+      param.push(isActive);
+    }
+
+    let sql = `INSERT INTO todos (${sqlColumn}) VALUES(${sqlValues})`
+
+    const result = await db.query(sql, param)
+
+    sql = 'SELECT * FROM todos WHERE todo_id = ?'
+    const row = (await db.query(sql, [result.insertId]))[0]
+
+    res.send({
+      "status": "Success",
+      "message": "Success",
+      "data": todoObject(row)
+    })
+  } else {
+    const missing = !title ? "title" : "activity_group_id"
+    res.status(400).send({
+      "status": "Bad Request",
+      "message": `${missing} cannot be null`,
+      "data": {}
+    });
+  }
+})
+
+// Read
+app.get('/todo-items', async (req, res) => {
+  const groupId = url.parse(req.url,true).query.activity_group_id;
+  console.log(groupId)
+
+  let sql = 'SELECT * FROM todos'
+  let values = []
+  if(groupId){
+    sql = sql.concat(' WHERE activity_group_id = ?')
+    values = [groupId]
+  } 
+  
+  const result = await db.query(sql, values)
+
+  res.send({
+    "status": "Success",
+    "message": "Success",
+    "data": result.map((row) => todoObject(row))
+  })
+})
+
+app.get('/todo-items/:id', async (req, res) => {
+  sql = 'SELECT * FROM todos WHERE todo_id = ?'
+  const result = await db.query(sql, [req.params.id])
+
+  if (result.length == 0) {
+    todoNotFound(req, res);
+  }
+  else {
+    res.send({
+      "status": "Success",
+      "message": "Success",
+      "data": todoObject(result[0])
+    })
+  }
+})
+
+// Update
+app.patch('/todo-items/:id', async (req, res) => {
+  const title = req.body.title;
+  const priority = req.body.priority;
+  const groupId = req.body.activity_group_id;
+  const isActive = req.body.is_active;
+
+  let sql = "SELECT * FROM todos WHERE todo_id = ?";
+  let row = await db.query(sql, [req.params.id])
+
+  if (row.length == 0) {
+    todoNotFound(req, res);
+  }
+
+
+  try {
+    sql = 'UPDATE todos SET '
+    let params = []
+    if (title) {
+      sql = sql.concat('title = ? ')
+      params.push(title)
+    }
+
+    if (priority) {
+      sql = sql.concat('priority = ? ');
+      params.push(priority);
+    }
+
+    if (groupId) {
+      sql = sql.concat('activity_group_id = ? ');
+      params.push(groupId);
+    }
+
+    if (isActive) {
+      sql = sql.concat('is_active = ? ');
+      params.push(isActive)
+    }
+    sql = sql.concat('WHERE todo_id = ?')
+    params.push(req.params.id)
+
+    const result = await db.query(sql, params)
+    console.log(result);
+  } catch (error) {
+    console.log(error);
+  }
+
+  sql = 'SELECT * FROM todos WHERE todo_id = ?'
+  row = (await db.query(sql, [req.params.id]))[0]
+  console.log(row);
+  res.send({
+    "status": "Success",
+    "message": "Success",
+    "data": todoObject(row)
+  })
+})
+
+// DELETE
+app.delete('/todo-items/:id', async (req, res) => {
+  let sql = "DELETE FROM todos WHERE todo_id = ?";
+  const result = await db.query(sql, [req.params.id])
+  if (result.affectedRows != 0) {
+    res.send({
+      "status": "Success",
+      "message": "Success",
+      "data": {}
+    })
+  }
+  else {
+    todoNotFound(req, res);
+  }
+})
+
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`TodoList app listening on port ${port}`)
 })
