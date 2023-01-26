3c7f2dc76be16fa3f8cf3ad54868c3e19e90ffbc
Agung Wicaksono <agungwck99@gmail.com>
2023-01-26 13:49:05 +0700

    feat: add create table

--- db.js
+++ db.js
@@ -1,6 +1,25 @@
 const mysql = require('mysql');
 require('dotenv').config()
 
+console.log(process.env)
+const createTableTodos = `CREATE TABLE IF NOT EXISTS todos (
+    todo_id integer PRIMARY KEY AUTO_INCREMENT,
+    activity_group_id integer,
+    title text,
+    priority text,
+    is_active boolean DEFAULT TRUE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+`
+const createTableActivities = `CREATE TABLE IF NOT EXISTS activities (
+    activity_id integer PRIMARY KEY AUTO_INCREMENT,
+    title text,
+    email varchar(255),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+`
 var con = mysql.createConnection({
     host: process.env.MYSQL_HOST,
     port: process.env.MYSQL_PORT,
@@ -12,6 +31,14 @@ var con = mysql.createConnection({
 con.connect(function (err) {
     if (err) throw err;
     console.log("Connected!");
+    con.query(createTableActivities, [], (err, result) => {
+        if (err) throw err;
+        else console.log(result);
+    });
+    con.query(createTableTodos, [], (err, result) => {
+        if (err) throw err;
+        else console.log(result);
+    });
 });
 
 const query = async (sql, params = []) => {
